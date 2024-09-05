8c4e7c5c78905e43725e94f4f9b7e32c4e0ac7e4
golanguzb71 <abdulaziomonov4@gmail.com>
2024-09-05 07:25:10 +0500

    BM_TREE

---
+++ Easy/Merge Sorted Array/main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+
+
+Solution().merge([1, 2, 3, 0, 0, 0], 3, [2, 6, 5], 3)
