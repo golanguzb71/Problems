3e059c6f1bda6cf7bd96218e032ff36b14bf85ff
abdulaziz omonov <abdulaziomonov4@gmail.com>
2025-01-27 07:52:51 +0500

    optimized 2 sum

--- Easy/two-sum/main.go
+++ Easy/two-sum/main.go
@@ -3,13 +3,16 @@ package main
 // https://leetcode.com/problems/two-sum/description/
 
 func twoSum(nums []int, target int) []int {
-	for i := range nums {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	m := make(map[int]int)
+	for i, num := range nums {
+		m[num] = i
+	}
+
+	for i, num := range nums {
+		complement := target - num
+		if j, ok := m[complement]; ok {
+			return []int{i, j}
 		}
 	}
 	return []int{}
 }
-
